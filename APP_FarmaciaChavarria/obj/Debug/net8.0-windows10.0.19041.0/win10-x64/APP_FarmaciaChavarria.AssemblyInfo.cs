f5f90eb26e73a9a484dae190235f727cc5de3ac7
Yonjar <juan132y@gmail.com>
2025-04-18 18:55:51 -0600

    feat: Se ha realizado el responsive de las interfaces

--- APP_FarmaciaChavarria/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/APP_FarmaciaChavarria.AssemblyInfo.cs
+++ APP_FarmaciaChavarria/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/APP_FarmaciaChavarria.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APP_FarmaciaChavarria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+67bdfa01770865eb45f8a5a47363a7d7c66f9824")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ede031f10897014da5f194d6d8a275d41df0759a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APP_FarmaciaChavarria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APP_FarmaciaChavarria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
